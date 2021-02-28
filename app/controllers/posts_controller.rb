1cf70ea909abafe4c940fbaaecd4ccf1a942351a
mor-ly246 <79776995+mor-ly246@users.noreply.github.com>
2021-02-28 13:23:45 +0900

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,11 @@
+class PostsController < ApplicationController
+   def index
+     @posts = Post.all  # １番目のレコードを@postに代入
+      end
+
+      def new
+      end
+      def create
+        Post.create(content: params[:content])
+      end
+ end
